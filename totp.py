f43365955d5e3fd012e240c2849226371090dc84
spookybean <71144171+spookybean@users.noreply.github.com>
2022-05-27 20:23:56 +0530

    totp verification after expiration time has elapsed

--- totp.py
+++ totp.py
@@ -1,7 +1,11 @@
 import pyotp
-
+import time
 
 totp = pyotp.TOTP('WH5X4NKE4JUPWUVFRMM4E3KEOHTO6KWD', interval=5)
 
 otp = totp.now()
 print(otp)
+
+print(totp.verify(totp.now()))
+time.sleep(5)
+print(totp.verify(totp.now()))
