16ee25fef8683fdd6fc47d842c27e65753f3197b
spookybean <71144171+spookybean@users.noreply.github.com>
2022-06-02 21:28:02 +0530

    async function to retrieve documents

--- motor_db.py
+++ motor_db.py
@@ -10,3 +10,17 @@ load_dotenv()
 client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv('MONGODB_URI'))
 
 db = client['test']
+
+
+async def example():
+    try:
+        document = await db.user.find_one({}, {'_id': 0})
+    except ServerSelectionTimeoutError as e:
+        print('db connection failed')
+    else:
+        print(document)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(example())
