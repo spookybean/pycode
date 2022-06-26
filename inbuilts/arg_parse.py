0081a72c7a6696fb13c65af855275f4a3b9a6656
spookybean <71144171+spookybean@users.noreply.github.com>
2022-06-26 12:49:00 +0530

    arguments handled

--- inbuilts/arg_parse.py
+++ inbuilts/arg_parse.py
@@ -5,3 +5,5 @@ parser = argparse.ArgumentParser(description='add an url')
 parser.add_argument('-u', '--url', metavar='', help='download url', required=True)
 
 args = parser.parse_args()
+
+print(args.url)
